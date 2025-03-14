4acd1fe80bef8fb50366042d5209ca448d85f254
Leanhquan <leanhquanvn97@gmail.com>
2025-03-14 19:30:15 +0700

    Add delay color

--- src/components/Map/TrainMarker.jsx
+++ src/components/Map/TrainMarker.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import { OverlayView } from "@react-google-maps/api";
 
 const TrainMarker = ({train, setSelectedTrain}) => {
+    const calculateRed = (delay) => {
+        switch (delay) {
+            case "5":
+                return "bg-red-50";
+            case "10":
+                return "bg-red-100";
+            case "15":
+                return "bg-red-200";
+            case "20":
+                return "bg-red-300";
+            default:
+                return "bg-red-400";
+        }
+    };
+    const color = train.delay.includes("delay") ? calculateRed(train.delay.split(' ')[0]) : "bg-green-500";
+
     return (
         <OverlayView
             position={train.position}
             mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
         >
-            <div className="bg-white w-min rounded border whitespace-nowrap p-1 rounded-tl-none"
+            <div className={`w-min rounded border whitespace-nowrap p-1 rounded-tl-none ${color}`}
                 onClick={() => setSelectedTrain(train)}
             >
                 <span className="font-bold">{train.trainNumber}</span><br/>
