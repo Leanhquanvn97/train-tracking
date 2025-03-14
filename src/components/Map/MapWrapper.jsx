51df60fece17a2908e373cb2219916ce5ac698cc
Leanhquan <leanhquanvn97@gmail.com>
2025-03-14 18:46:06 +0700

    Set a custom marker for train

--- src/components/Map/MapWrapper.jsx
+++ src/components/Map/MapWrapper.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import TrainMarker from './TrainMarker';
 
 const containerStyle = {
-	height: "600px",
-	width: "600px"
+	height: "800px",
+	width: "800px"
 };
 
 const markerPositions = [
@@ -15,13 +16,24 @@ const markerPositions = [
 	{ lat: 53.5511, lng: 9.9937 },
 ];
 
+const trainData = [
+	{ id: 1, trainNumber: 'ICE 123', city: 'Berlin', delay: 'On Time', position: { lat: 52.5200, lng: 13.4050 } },
+	{ id: 2, trainNumber: 'ICE 456', city: 'Frankfurt', delay: '5 min delay', position: { lat: 50.1109, lng: 8.6821 } },
+	{ id: 3, trainNumber: 'ICE 789', city: 'Hamburg', delay: '10 min delay', position: { lat: 53.1, lng: 9.9937 } },
+]
+
+
 const MapWrapper = () => {
 	const { isLoaded } = useJsApiLoader({
 		id: 'google-map-script',
 		googleMapsApiKey: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
-	})
+	});
 
-	const [map, setMap] = React.useState(null)
+	const [map, setMap] = React.useState(null);
+	const [selectedTrain, setSelectedTrain] = useState(null);
+	const setSelectedTrainCallback = useCallback((train) => {
+		setSelectedTrain(train);
+	}, []);
 
 	const onLoad = React.useCallback(function callback(map) {
 		const bounds = new window.google.maps.LatLngBounds()
@@ -29,11 +41,11 @@ const MapWrapper = () => {
 		map.fitBounds(bounds)
 
 		setMap(map)
-	}, [])
+	}, []);
 
 	const onUnmount = React.useCallback(function callback(map) {
 		setMap(null)
-	}, [])
+	}, []);
 
 	return isLoaded ? (
 		<GoogleMap
@@ -44,10 +56,13 @@ const MapWrapper = () => {
 			{markerPositions.map((position, index) => (
 				<Marker key={index} position={position} />
 			))}
+			{trainData.map(train => (
+				<TrainMarker train={train} setSelectedTrain={setSelectedTrainCallback} key={train.id}></TrainMarker>
+			))}
 		</GoogleMap>
 	) : (
 		<></>
-	)
-};
+	);
+}
 
 export default React.memo(MapWrapper);
---
+++ src/components/Map/TrainMarker.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { OverlayView } from "@react-google-maps/api";
+
+const TrainMarker = ({train, setSelectedTrain}) => {
+    return (
+        <OverlayView
+            position={train.position}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        >
+            <div className="bg-white w-min rounded border whitespace-nowrap p-1"
+                onClick={() => setSelectedTrain(train)}
+            >
+                <span className="font-bold">{train.trainNumber}</span><br/>
+                {train.city}<br/>
+                {train.delay}
+            </div>
+        </OverlayView>
+    )
+}
+
+export default React.memo(TrainMarker);
